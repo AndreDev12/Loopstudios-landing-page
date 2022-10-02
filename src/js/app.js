c067c298b178a5f297309e951c8b8cc5387a9fc5
André Torres <andretorresjava@gmail.com>
2022-10-02 13:09:26 -0500

    Adding the function of showing the mobile menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
 import '../scss/app.scss';
+
+const menuButton = document.querySelector('.menu-button');
+
+menuButton.addEventListener('click', showMobileMenu);
+
+function showMobileMenu(){
+    if(menuButton.classList.contains('active')){
+        this.classList.toggle('active');
+        return;
+    }
+    this.classList.toggle('active');
+}
