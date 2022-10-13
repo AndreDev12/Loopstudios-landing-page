3fabadd84b039ca10d7e44e819afd905a200f0a3
André Torres <andretorresjava@gmail.com>
2022-10-13 18:07:06 -0500

    Add the logic to menu button

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,19 @@
 import '../scss/app.scss';
 
 const menuButton = document.querySelector('.menu-button');
+const header = document.querySelector('.header');
+const body = document.querySelector('body');
 
 menuButton.addEventListener('click', showMobileMenu);
 
 function showMobileMenu(){
     if(menuButton.classList.contains('active')){
         this.classList.toggle('active');
+        header.classList.add('background-image');
         return;
     }
     this.classList.toggle('active');
+    // header.classList.add('black-background');
+    body.classList.add('black-background');
+    header.classList.remove('background-image');
 }
