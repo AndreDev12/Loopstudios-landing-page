2b6f8db48b5aa6c0557cb2430ec219ebfb6e928f
André Torres <andretorresjava@gmail.com>
2022-10-27 12:16:21 -0500

    Modify the index.html file

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,16 @@
 import '../scss/app.scss';
 
-const menuButton = document.querySelector('.menu-button');
-const header = document.querySelector('.header');
-const body = document.querySelector('body');
-// const bodyBefore = document.querySelector(body > body::before);
-// console.log(bodyBefore);
+const hamburguerMenu = document.querySelector('.hamburguer-menu');
 
 eventListeners();
 function eventListeners(){
-    menuButton.addEventListener('click', showMobileMenu);
+    hamburguerMenu.addEventListener('click', showMobileMenu);
 }
 
 function showMobileMenu(){
-    if(menuButton.classList.contains('active')){
+    if(this.classList.contains('active')){
         this.classList.toggle('active');
-        // body.classList.remove('black-background');
         return;
     }
     this.classList.toggle('active');
-    const response = window.getComputedStyle(document.querySelector('.header', '::before'));
-    // response.position = "fixed";
-    // response.classList.add('fixed');
-    console.log(response.setProperty);
-    // headerbefore.classList.add('fixed');
-    // body.classList.add('black-background');
 }
