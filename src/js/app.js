e23c57bf4ede4505f80575ae47b55925ae294886
André Torres <andretorresjava@gmail.com>
2022-10-31 13:18:33 -0500

    Add styles to the mobile menu

--- src/js/app.js
+++ src/js/app.js
@@ -12,8 +12,10 @@ function showMobileMenu(){
     if(this.classList.contains('active')){
         this.classList.toggle('active');
         headerNav.classList.remove('header-nav--active');
+        document.body.style.overflow = "initial";
         return;
     }
+    document.body.style.overflow = "hidden";
     headerNav.classList.toggle('header-nav--active');
     this.classList.toggle('active');
 }
